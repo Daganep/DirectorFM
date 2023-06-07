aede7f948066b054fec557e11cc8fcee781b6847
Daganep <daganep@yandex.ru>
2023-06-07 16:57:03 +0300

    Add home screen instead main screen

--- DirectorFM/app/src/main/java/com/openkin/directorfm/ui/screens/MainScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.openkin.directorfm.ui.screens
-
-import androidx.compose.runtime.Composable
-import com.openkin.directorfm.viewmodels.MainScreenViewModel
-import org.koin.androidx.compose.koinViewModel
-
-@Composable
-fun MainScreen(viewModel: MainScreenViewModel = koinViewModel()) {
-}
